6d6e63b72e93a5a4ddd612008f03084ef9b731cd
Potassium <brainiacgamer444@gmail.com>
2023-12-04 00:01:28 +0545

    Create print function to print in console

--- script.js
+++ script.js
@@ -5,15 +5,18 @@ function createRandomNumber() {
   return Array.from({ length: size }, () => Math.floor(Math.random() * 100));
 }
 
+function print(tree) {
+  console.log("Level order:", [tree.levelOrder().join(", ")]);
+  console.log("Preorder:", [tree.preOrder().join(", ")]);
+  console.log("Postorder:", [tree.postOrder().join(", ")]);
+  console.log("Inorder:", [tree.inOrder().join(", ")]);
+}
+
 const BST = new BinaryTree(createRandomNumber());
 
 console.log("Is balanced:", BST.isBalanced());
 
-console.log("Level order:", [BST.levelOrder().join(", ")]);
-console.log("Preorder:", [BST.preOrder().join(", ")]);
-console.log("Postorder:", [BST.postOrder().join(", ")]);
-console.log("Inorder:", [BST.inOrder().join(", ")]);
-// print(BST);
+print(BST);
 
 BST.insert(150);
 BST.insert(200);
@@ -27,4 +30,4 @@ console.log("Preorder:", [BST.preOrder().join(", ")]);
 console.log("Postorder:", [BST.postOrder().join(", ")]);
 console.log("Inorder:", [BST.inOrder().join(", ")]);
 
-// print(BST);
+print(BST);
